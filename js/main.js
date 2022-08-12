80c1d81acbf43d057c2d07e25a58ea625676d535
zinnycode <zinny01@gmail.com>
2022-08-12 15:24:44 +0200

    i have worked on the media query

--- js/main.js
+++ js/main.js
@@ -3,12 +3,17 @@ const closeMenu = document.querySelector(".navClose")
 const navMenu = document.querySelector(".navLinks")
 const logo1 = document.querySelector(".logo1")
 const faqs = document.querySelectorAll(".faq")
+const navlinks = document.querySelectorAll(".navLinks a")
+const html = document.querySelector("html")
+
+console.log(navlinks)
 
 
 openMenu.addEventListener("click",function(){
     navMenu.style.top = "0"
     openMenu.style.display = "none"
     logo1.style.display = "none"
+    html.style.overflowY = "hidden"
 
 })
 
@@ -16,10 +21,20 @@ closeMenu.addEventListener("click", function(){
     navMenu.style.top = "-100%"
     openMenu.style.display = "block"
     logo1.style.display = "block"
+    html.style.overflowY = "scroll"
 })
 
 faqs.forEach(function(faq){
     faq.addEventListener('click', function(){
         faq.classList.toggle("showAns")
+    })
+})
+
+navlinks.forEach(function(navlink){
+    navlink.addEventListener("click", function(){
+        navMenu.style.top = "-100%"
+        openMenu.style.display = "block"
+        logo1.style.display = "block"
+        html.style.overflowY = "scroll"
     })
 })
